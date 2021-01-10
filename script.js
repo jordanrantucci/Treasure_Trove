9ded6eb30fb94064acff1dfa0d8b0c8ddc5d2c29
Diane Furlong <dfurlongm@gmail.com>
2021-01-09 22:35:23 -0500

    added bing maps ajax request

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
-    
+    //Bing Maps API
+    let apiKey = config.key
+    let userLocation = ""
+    let queryURL = "https://bing.com/maps/default.aspx?cp="+userLocation+"&style=h"+"&sp=point."+latitude+"_"+longitude+"_"+picTitle+"_"+refURL+"_"+photoURL
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        
+    })
 })
