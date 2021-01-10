1e6f7e1810f42ade463d15362dee06414e3b23d2
Diane Furlong <dfurlongm@gmail.com>
2021-01-09 23:57:29 -0500

    added labels for needed info from Gyazo and did some rearranging

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     //Bing Maps API
     let apiKey = config.key
     let ipapiURL = "https://ipapi.co/8.8.8.8/json/"
+    let gyazoURL = "" //nick
 
 
     $.ajax({
@@ -9,18 +10,24 @@ $(document).ready(function() {
         method: "GET"
     }).then(function(response){
         let userLocation = response.latitude+"~"+response.longitude
-        let pointLatitude = ""
-        let pointLongitude = ""
-        let picTitle = ""
-        let refURL = ""
-        let photoURL = ""
-        let queryURL = "https://bing.com/maps/default.aspx?cp="+userLocation+"&style=h"+"&sp=point."+pointLatitude+"_"+pointLongitude+"_"+picTitle+"_"+refURL+"_"+photoURL
-        
         $.ajax({
-            url: queryURL,
-            method= "GET"
+            url: gyazoURL="",
+            method: "GET"
         }).then(function(response2){
-            
+        let pointLatitude = response2.something //nick
+        let pointLongitude = response2.something //nick
+        let picTitle = response2.something //nick again
+        let refURL = response2.something //and again
+        let photoURL = response2.something//last one
+        let bingMapURL = "https://bing.com/maps/default.aspx?cp="+userLocation+"&style=h"+"&sp=point."+pointLatitude+"_"+pointLongitude+"_"+picTitle+"_"+refURL+"_"+photoURL
+        
+            $.ajax({
+                url: bingMapURL,
+                method= "GET"
+            }).then(function(response3){
+                let mapDiv = $("<div>")
+                
+            })
         })
     })
 })
